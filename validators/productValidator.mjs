fab73dd5439d29b734c8046b41df3889029c9809
MARVIN <o.silverygold@gmail.com>
2024-08-13 15:46:15 +0300

    create product validator

---
+++ validators/productValidator.mjs
@@ -0,0 +1,79 @@
+import { checkSchema } from "express-validator";
+import Category from "../src/models/category.mjs";
+
+const checkCategoryExists = async (categoryId) => {
+  if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+    return Promise.reject("Invalid category ID");
+  }
+  const category = await Category.findById(categoryId);
+  if (!category) {
+    return Promise.reject("Category does not exist");
+  }
+  return true;
+};
+
+export const productValidator = checkSchema({
+  name: {
+    escape: true,
+    trim: true,
+    notEmpty: {
+      errorMessage: "Name must be present",
+    },
+    isString: {
+      errorMessage: "Name must be a string",
+    },
+    isLength: {
+      options: { min: 3 },
+      errorMessage: "Name must be at least 3 chars long",
+    },
+  },
+  price: {
+    notEmpty: {
+      errorMessage: "Price must be set",
+    },
+    isNumeric: {
+      errorMessage: "Price must be a number",
+    },
+    isFloat: {
+      options: { min: 0 },
+      errorMessage: "Price must be a positive number",
+    },
+    toFloat: true,
+  },
+  quantity: {
+    notEmpty: {
+      errorMessage: "Quantity must be set",
+    },
+    isNumeric: {
+      errorMessage: "Quantity must be a number",
+    },
+    isInt: {
+      options: { min: 0 },
+      errorMessage: "Quantity must be a positive whole number",
+    },
+    toInt: true,
+  },
+  description: {
+    escape: true,
+    trim: true,
+    notEmpty: {
+      errorMessage: "Description must be present",
+    },
+    isString: {
+      errorMessage: "Description must be a string",
+    },
+    isLength: {
+      options: { min: 5 },
+      errorMessage: "Description must be at least 3 chars long",
+    },
+  },
+  category: {
+    notEmpty: {
+      errorMessage: "Description must be present",
+    },
+    custom: {
+      options: checkCategoryExists,
+      errorMessage: "Invalid/Non-existent category",
+    },
+  },
+});
