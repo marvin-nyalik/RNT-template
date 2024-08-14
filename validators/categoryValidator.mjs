bf4c77d3452d08644a92eba3b7f77b0ee46d2f7a
MARVIN <o.silverygold@gmail.com>
2024-08-14 12:40:28 +0300

    Add description field validator

--- validators/categoryValidator.mjs
+++ validators/categoryValidator.mjs
@@ -2,6 +2,11 @@ import { checkSchema } from "express-validator";
 
 export const categoryValidator = checkSchema({
   name: {
+    escape: true,
+    trim: true,
+    notEmpty: {
+      errorMessage: "Name must be present",
+    },
     isString: {
       errorMessage: "Name must be a string",
     },
@@ -9,10 +14,19 @@ export const categoryValidator = checkSchema({
       options: { min: 3 },
       errorMessage: "Name must be at least 3 chars long",
     },
+  },
+  description: {
     escape: true,
     trim: true,
     notEmpty: {
-      errorMessage: "Name must be present",
+      errorMessage: "description must be present",
+    },
+    isString: {
+      errorMessage: "description must be a string",
+    },
+    isLength: {
+      options: { min: 5 },
+      errorMessage: "description must be at least 3 chars long",
     },
   },
 });
