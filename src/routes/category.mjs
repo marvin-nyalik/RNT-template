fe945c8d6038550de4d4512741c99f55ddad1f54
MARVIN <o.silverygold@gmail.com>
2024-08-14 12:43:25 +0300

    finish category routers with validation

---
+++ src/routes/category.mjs
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { categoryValidator } from "../../validators/categoryValidator.mjs";
+import {
+  createCategory,
+  updateCategory,
+  deleteCategory,
+  getCategories,
+  getCategoryById,
+} from "../controllers/categories.mjs";
+
+const router = Router();
+
+router.post("/", categoryValidator, createCategory);
+router.patch("/:id", categoryValidator, updateCategory);
+router.delete("/:id", deleteCategory);
+router.get("/", getCategories);
+router.get("/:id", getCategoryById);
+
+export default router;
