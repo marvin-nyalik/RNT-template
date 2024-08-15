0e86e1a85602162c02edc8cbe1298d15882e1ba7
MARVIN <o.silverygold@gmail.com>
2024-08-15 16:49:04 +0300

    add `upload` middleware in product creation pipeline

--- src/routes/product.mjs
+++ src/routes/product.mjs
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} from "../controllers/products.mjs";
+import { productValidator } from "../../validators/productValidator.mjs";
+import { upload } from "../middlewares/upload.mjs";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.status(200).send('Hi products');
-})
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+router.post("/", upload.single("image"), productValidator, createProduct);
+router.patch("/", productValidator, updateProduct);
+router.post("/image", upload.single("avatar"), (req, res) => {
+  res.json(req.file);
+});
+router.delete("/:id", deleteProduct);
 
 export default router;
