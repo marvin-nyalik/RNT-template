7a3e6db5b4036444d4205dedff861962dd808876
MARVIN <o.silverygold@gmail.com>
2024-08-16 20:13:33 +0300

    Add userRouter to index router

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import categoryRouter from "./category.mjs";
 import productRouter from "./product.mjs";
+import userRouter from "./user.mjs";
 
 const router = Router();
 
 router.use("/products", productRouter);
 router.use("/categories", categoryRouter);
+router.use("/users", userRouter);
 
 export default router;
