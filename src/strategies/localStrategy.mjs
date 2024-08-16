730816cb8c42bf92be9c59f7dbacb22365854291
MARVIN <o.silverygold@gmail.com>
2024-08-16 20:11:12 +0300

    Define local auth strategy

---
+++ src/strategies/localStrategy.mjs
@@ -0,0 +1,39 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import User from "../models/user.mjs";
+import bcrypt from "bcrypt";
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    if (!user) return done(new Error("User not found"), null);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+const localAuth = passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+      if (!user) {
+        return done(null, false, { message: "User not found" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return done(null, false, { message: "Invalid credentials" });
+      }
+      done(null, user);
+    } catch (error) {
+      done(error, null);
+    }
+  })
+);
+
+export default localAuth;
