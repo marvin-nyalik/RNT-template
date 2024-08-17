a335ca36df500658e69dce47d95cbb20d9712486
MARVIN <o.silverygold@gmail.com>
2024-08-17 19:40:47 +0300

    Add auth routes

--- src/routes/auth.mjs
+++ src/routes/auth.mjs
@@ -1,8 +1,26 @@
 import { Router } from "express";
-import localAuth from '../strategies/localStrategy.mjs';
-import googleAuth from '../strategies/googleStrategy.mjs';
+import "../strategies/localStrategy.mjs";
+import "../strategies/googleStrategy.mjs";
 import passport from "passport";
 
 const router = Router();
+
+router.post("/local/auth", passport.authenticate("local"), (req, res) => {
+  res.status(200).json({ msg: "Authenticated", user: req.user });
+});
+
+router.get(
+  "/google/auth",
+  passport.authenticate("google", {
+    scope: ["email", "profile"],
+  }),
+  (req, res) => {
+    res.status(200).json({ msg: "Authenticated" });
+  }
+);
+
+router.get("/auth/google", passport.authenticate("google"), (req, res) => {
+  res.status(200).json({ msg: "Authenticated", user: req.user });
+});
 
 export default router;
