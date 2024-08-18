e6cfd6ce36f692e22fdf16ce6c7e8a4cf70d0a07
MARVIN <o.silverygold@gmail.com>
2024-08-18 16:04:59 +0300

    configure google strategy for export

--- src/strategies/googleStrategy.mjs
+++ src/strategies/googleStrategy.mjs
@@ -5,42 +5,43 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+export const configurePassport = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    if (!user) return done(new Error("User not found"), null);
-    done(null, user);
-  } catch (error) {
-    done(error, null);
-  }
-});
-
-const googleAuth = passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: process.env.CALLBACK_URL,
-    },
-    async (request, token, tokenSecret, profile, done) => {
-      try {
-        let user = await User.findOne({ googleId: profile.id });
-        if (!user) {
-          user = await User.create({
-            googleId: profile.id,
-            username: profile.displayName,
-            email: profile.emails[0].value,
-          });
-        }
-      } catch (error) {
-        done(error, null);
-      }
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      if (!user) return done(new Error("User not found"), null);
+      done(null, user);
+    } catch (error) {
+      done(error, null);
     }
-  )
-);
+  });
 
-export default googleAuth;
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: process.env.CALLBACK_URL,
+      },
+      async (request, token, tokenSecret, profile, done) => {
+        try {
+          let user = await User.findOne({ googleId: profile.id });
+          if (!user) {
+            user = await User.create({
+              googleId: profile.id,
+              username: profile.emails[0].value,
+              email: profile.emails[0].value,
+            });
+          }
+          done(null, user);
+        } catch (error) {
+          done(error, null);
+        }
+      }
+    )
+  );
+};
