a9ece95aaa96073633662681a8b5ba68b3811c77
MARVIN <o.silverygold@gmail.com>
2024-08-18 16:07:32 +0300

    use auth router

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -2,11 +2,13 @@ import { Router } from "express";
 import categoryRouter from "./category.mjs";
 import productRouter from "./product.mjs";
 import userRouter from "./user.mjs";
+import authRouter from "./auth.mjs";
 
 const router = Router();
 
 router.use("/products", productRouter);
 router.use("/categories", categoryRouter);
 router.use("/users", userRouter);
+router.use("/auth", authRouter);
 
 export default router;
