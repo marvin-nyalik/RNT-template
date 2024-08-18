340df039d7d43cf3906ed35f683258d9965e6c26
MARVIN <o.silverygold@gmail.com>
2024-08-18 16:05:15 +0300

    config/export local strategy

--- src/strategies/localStrategy.mjs
+++ src/strategies/localStrategy.mjs
@@ -3,37 +3,37 @@ import { Strategy as LocalStrategy } from "passport-local";
 import User from "../models/user.mjs";
 import bcrypt from "bcrypt";
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+export const initializePassport = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    if (!user) return done(new Error("User not found"), null);
-    done(null, user);
-  } catch (error) {
-    done(error, null);
-  }
-});
-
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
+  passport.deserializeUser(async (id, done) => {
     try {
-      const user = await User.findOne({ username });
-      if (!user) {
-        return done(null, false, { message: "User not found" });
-      }
-
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return done(null, false, { message: "Invalid credentials" });
-      }
+      const user = await User.findById(id);
+      if (!user) return done(new Error("User not found"), null);
       done(null, user);
     } catch (error) {
       done(error, null);
     }
-  })
-);
+  });
 
-export default passport;
+  passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username });
+        if (!user) {
+          return done(null, false, { message: "User not found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: "Invalid credentials" });
+        }
+        done(null, user);
+      } catch (error) {
+        done(error, null);
+      }
+    })
+  );
+};
