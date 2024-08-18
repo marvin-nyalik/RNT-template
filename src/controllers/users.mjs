d2dac6d3f477782599ad747c23c0baa9d4367278
MARVIN <o.silverygold@gmail.com>
2024-08-18 16:06:06 +0300

    use bcrypt helpers for pwd encryption

--- src/controllers/users.mjs
+++ src/controllers/users.mjs
@@ -1,19 +1,24 @@
 import User from "../models/user.mjs";
 import { validationResult, matchedData } from "express-validator";
-import bcrypt from "bcrypt";
-import { hashPassword, comparePassword } from "../utils/helpers.mjs";
+import { hashPassword } from "../utils/helpers.mjs";
 
 export const registerUser = async (req, res) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty())
+  if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
+  }
 
   const data = matchedData(req);
-  console.log(data)
+
   try {
-    if (data.password) data.password = hashPassword(data.password)
+    if (data.password) {
+      data.password = await hashPassword(data.password);
+    }
+
     const user = await User.create(data);
-    if (!user) return res.status(403).json({ message: "Forbidden" });
+    if (!user) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
 
     const userResponse = {
       username: user.username,
@@ -22,7 +27,7 @@ export const registerUser = async (req, res) => {
 
     return res.status(201).json(userResponse);
   } catch (error) {
-    console.log("Error registering user: ", error);
+    console.log("Error registering user:", error);
     res
       .status(500)
       .json({ message: "A server error prevented registration", error });
