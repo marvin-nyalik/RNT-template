a9161667a524a02478bd549742867333d81a56aa
MARVIN <o.silverygold@gmail.com>
2024-08-20 18:08:17 +0300

    update product validator schema

--- src/validators/productValidator.mjs
+++ src/validators/productValidator.mjs
@@ -9,88 +9,87 @@ const checkCategoryExists = async (categoryId) => {
   return true;
 };
 
-export const productValidator = (isUpdate = false) =>
-  checkSchema({
-    name: {
-      escape: true,
-      trim: true,
-      optional: isUpdate,
-      notEmpty: {
-        errorMessage: "Name must be present",
-      },
-      isString: {
-        errorMessage: "Name must be a string",
-      },
-      isLength: {
-        options: { min: 3 },
-        errorMessage: "Name must be at least 3 chars long",
-      },
+export const productValidator = checkSchema({
+  name: {
+    escape: true,
+    trim: true,
+    required: true,
+    notEmpty: {
+      errorMessage: "Name must be present",
     },
-    price: {
-      optional: isUpdate,
-      notEmpty: {
-        errorMessage: "Price must be set",
-      },
-      isNumeric: {
-        errorMessage: "Price must be a number",
-      },
-      isFloat: {
-        options: { min: 0 },
-        errorMessage: "Price must be a positive number",
-      },
-      toFloat: true,
+    isString: {
+      errorMessage: "Name must be a string",
     },
-    stockThreshold: {
-      optional: isUpdate,
-      notEmpty: {
-        errorMessage: "Stock threshold must be set",
-      },
-      isNumeric: {
-        errorMessage: "Stock threshold must be a number",
-      },
-      isFloat: {
-        options: { min: 10 },
-        errorMessage: "Stock threshold must be a positive number",
-      },
-      toFloat: true,
+    isLength: {
+      options: { min: 3 },
+      errorMessage: "Name must be at least 3 chars long",
     },
-    quantity: {
-      optional: isUpdate,
-      notEmpty: {
-        errorMessage: "Quantity must be set",
-      },
-      isNumeric: {
-        errorMessage: "Quantity must be a number",
-      },
-      isInt: {
-        options: { min: 0 },
-        errorMessage: "Quantity must be a positive whole number",
-      },
-      toInt: true,
+  },
+  price: {
+    required: true,
+    notEmpty: {
+      errorMessage: "Price must be set",
     },
-    description: {
-      escape: true,
-      trim: true,
-      optional: isUpdate,
-      notEmpty: {
-        errorMessage: "Description must be present",
-      },
-      isString: {
-        errorMessage: "Description must be a string",
-      },
-      isLength: {
-        options: { max: 500 },
-        errorMessage: "Description cannot exceed 500 chars",
-      },
+    isNumeric: {
+      errorMessage: "Price must be a number",
     },
-    category: {
-      optional: isUpdate,
-      notEmpty: {
-        errorMessage: "Category must be present",
-      },
-      custom: {
-        options: checkCategoryExists,
-        errorMessage: "Invalid/Non-existent category",
-      },
+    isFloat: {
+      options: { min: 0 },
+      errorMessage: "Price must be a positive number",
     },
-  });
+    toFloat: true,
+  },
+  stockThreshold: {
+    required: true,
+    notEmpty: {
+      errorMessage: "Stock threshold must be set",
+    },
+    isNumeric: {
+      errorMessage: "Stock threshold must be a number",
+    },
+    isFloat: {
+      options: { min: 10 },
+      errorMessage: "Stock threshold must be a positive number",
+    },
+    toFloat: true,
+  },
+  quantity: {
+    required: true,
+    notEmpty: {
+      errorMessage: "Quantity must be set",
+    },
+    isNumeric: {
+      errorMessage: "Quantity must be a number",
+    },
+    isInt: {
+      options: { min: 0 },
+      errorMessage: "Quantity must be a positive whole number",
+    },
+    toInt: true,
+  },
+  description: {
+    escape: true,
+    trim: true,
+    required: true,
+    notEmpty: {
+      errorMessage: "Description must be present",
+    },
+    isString: {
+      errorMessage: "Description must be a string",
+    },
+    isLength: {
+      options: { max: 500 },
+      errorMessage: "Description cannot exceed 500 chars",
+    },
+  },
+  category: {
+    required: true,
+    notEmpty: {
+      errorMessage: "Category must be present",
+    },
+    custom: {
+      options: checkCategoryExists,
+      errorMessage: "Invalid/Non-existent category",
+    },
+  },
+});
