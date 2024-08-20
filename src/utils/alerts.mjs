06163c0b3f5b1812afdd2ef5ccbf92b7c48a1bce
MARVIN <o.silverygold@gmail.com>
2024-08-20 18:07:57 +0300

    create email `Stock Alert` template

--- src/utils/alerts.mjs
+++ src/utils/alerts.mjs
@@ -1,7 +1,11 @@
+import { mailOptions, sendMail, transporter } from "../config/nodemailer.mjs";
+
 export const sendStockAlert = (product) => {
-  const alertMessage = `Alert: Stock for ${product.name} 
-    is below the required threshold of ${product.stockThreshold}. 
-    Current quantity is ${product.quantity}.
-    `;
-  
+  const alertMessage = `
+    <p><strong>Alert:</strong> Stock for <strong>${product.name}</strong> 
+    is below the required threshold of <strong>${product.stockThreshold}</strong>. 
+    Current quantity is <strong>${product.quantity}</strong>.</p>
+  `;
+  const configOptions = {...mailOptions, html: alertMessage };
+  sendMail(transporter, configOptions);
 };
